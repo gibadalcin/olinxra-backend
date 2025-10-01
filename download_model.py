665ba8a28dc894a879ca51ce0395cef359fc32fb
Gilberto Dalcin <dalcin.tools@outlook.com>
2025-10-01 17:43:52 -0300

    fix: atualiza a rota das credenciais de acesso

--- download_model.py
+++ download_model.py
@@ -4,7 +4,7 @@ from google.cloud import storage
 bucket_name = "olinxra-modelos"
 source_blob_name = "quantized_clip_model.onnx"
 destination_file_name = "quantized_clip_model.onnx"
-cred_path = os.environ.get("GOOGLE_APPLICATION_CREDENTIALS", "cloud-storage-cred.json")
+cred_path = "models-downloader-cred.json"
 
 storage_client = storage.Client.from_service_account_json(cred_path)
 bucket = storage_client.bucket(bucket_name)
